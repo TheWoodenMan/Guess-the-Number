f92b44e18bacec12051bf46d98d4f87e646e30aa
Andy Wood <85075266+TheWoodenMan@users.noreply.github.com>
2022-03-13 17:52:03 +0000

    Functional Game
    The game is functional in a basic way and just needs to be optimised/adapted a little more with some features added.

--- script.js
+++ script.js
@@ -23,16 +23,46 @@ console.log(document.querySelector('.guess').value);
 // Dont forget to include the dot .
 */
 
-const x = function () {
-  console.log(23);
-};
+// This is the secret number and score, both are state variables
+// and are part of the score, as opposed to being in the DOM
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+// Event listener and button control.
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no input.
   if (!guess) {
     document.querySelector('.message').textContent = 'No number! 🚫';
+
+    // When player wins.
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🥳 Correct Number! 🎊';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (score > 1) {
+    // When guess is too high.
+    if (guess > secretNumber) {
+      document.querySelector('.message').textContent = 'Too High! ⬆️';
+      score--;
+      document.querySelector('.score').textContent = score;
+
+      // When guess is too low.
+    } else if (guess < secretNumber) {
+      document.querySelector('.message').textContent = 'Too Low! ⬇️';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+  } else {
+    document.querySelector('.message').textContent = 'You Lose the Game! 💀';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
 
@@ -43,3 +73,11 @@ document.querySelector('.check').addEventListener('click', function () {
 // !guess (no guess) defaults to 0 which is a falsy value and thus "true"
 // for the purpose of the if function due to the ! negation.
 // This covers the function for "no input" situations.
+
+// Always check typos or case sensitive first when debugging
+// You'll save a bunch of time and trouble.
+
+// Key-points for changing css from javascript
+// affect style.backgroundColor (for example)
+// two words needs to be camel case not hyphenated.
+// Needs to be a string, e.g. 'body'
