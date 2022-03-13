097c9a287dc2fc437d6f8a6f9a42122a547f68ca
Andy Wood <85075266+TheWoodenMan@users.noreply.github.com>
2022-03-13 16:11:38 +0000

    Button Logic and Functionality
    The button on the site was hooked up to the JS it now reports a message on no-input situations and logs the "guess" to the console when the input is a number.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # starter
- a simple numbers game project - Complete Javascript Course '22
+
+a simple numbers game project from The Complete Javascript Course '22 by Jonas Schmedtman jonas.io
+
+the HTML/CSS files were pre-written.
+The idea of this course is to code the javascript part of the site
+and learn how to connect that to the HTML/CSS front end.
+aka DOM (Document Object Model) Manipulation.
--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 'use strict';
+/*
+// querySelector aims at id (#) and class (.) components of the
+// HTML/CSS. In this case it's a paragraph (p)
+// textContent selects the contents of that paragraph.
+// When there are multiple .dot operators, they
+// execute from left to right.
+
+console.log(document.querySelector('.message').textContent);
+document;
+document.querySelector('.message').textContent = '🥳 Correct Number! 🎊';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+// Notes - document.querySelector refers to an element in the html doc. (class or id)
+// .textcontent refers to the text value of that element. .value refers to the number value.
+// Dont forget to include the dot .
+*/
+
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number! 🚫';
+  }
+});
+
+// addEventListener sits and waits for condition (click) to happen,
+// when it does happen the function detailed in part 2 of the expression
+// is called and executed.
+
+// !guess (no guess) defaults to 0 which is a falsy value and thus "true"
+// for the purpose of the if function due to the ! negation.
+// This covers the function for "no input" situations.
