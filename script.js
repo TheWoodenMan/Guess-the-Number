2f44f5967366128bc3fd2596a14a8caa66bf43b4
Andy Wood <85075266+TheWoodenMan@users.noreply.github.com>
2022-03-15 18:02:22 +0000

    Reset functionality
    hooked up the 'again' button to reset all variables and css to default when the game is over.

--- script.js
+++ script.js
@@ -26,10 +26,23 @@ console.log(document.querySelector('.guess').value);
 // This is the secret number and score, both are state variables
 // and are part of the score, as opposed to being in the DOM
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.message').textContent = 'Guess the Number?! 🧮';
 
 let score = 20;
+
+// Reset event listener and button control.
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Guess the Number?! 🧮';
+});
 
 // Event listener and button control.
 document.querySelector('.check').addEventListener('click', function () {
@@ -43,6 +56,7 @@ document.querySelector('.check').addEventListener('click', function () {
     // When player wins.
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🥳 Correct Number! 🎊';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -81,3 +95,19 @@ document.querySelector('.check').addEventListener('click', function () {
 // affect style.backgroundColor (for example)
 // two words needs to be camel case not hyphenated.
 // Needs to be a string, e.g. 'body'
+
+// Challenge - Implement a game reset function.
+// 1. Select the 'again' class and attach a click event handler.✅
+// 2. in the function, restore the initial values of 'score' and 'number'✅
+// 3. Restore the initial conditions of the message, number, score and guess input field. ✅
+// 4. also restore the original background colour (#222) and number width. ✅
+
+// <---Challenge Debug notes --->
+// Make sure class/id tags ./#
+// Double check brackets/curly are all open/close.
+// Changing a const is forbidden, needed to change to let.
+// 'guess' is only defined in the first event handler so we need to
+// directly affect the text content rather than just change the variable which isn't possible.
+// updating the 'score' variable doesn't automatically update the textcontent of score.
+// textcontent and value are not the same and setting textcontent to null or '' when its actually
+// value will do nothing. Value is for form contents, textcontent is for other html elements.
